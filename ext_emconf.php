32b85a9d635cdf5b53ec3af9885473b1c6a43984
Daniel Ablass <dnartists@web.de>
2022-01-25 16:54:51 +0100

    [INFO] First commit. [INFO] Classes moved from psb_foundation.

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+$EM_CONF['psb_debug'] = [
+    'author'           => 'Daniel Ablass',
+    'author_email'     => 'dn@phantasie-schmiede.de',
+    'category'         => 'misc',
+    'clearCacheOnLoad' => true,
+    'constraints'      => [
+        'conflicts' => [
+        ],
+        'depends'   => [
+            'php'   => '7.4',
+            'typo3' => '11.5.0-11.5.99',
+        ],
+        'suggests'  => [
+        ],
+    ],
+    'description'      => 'a collection of debugging tools',
+    'state'            => 'stable',
+    'title'            => 'PSbits | Debug',
+    'version'          => '1.0.0',
+];
