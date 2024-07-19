d53a649c89d9b80d0865649e0120f6391c9bd5a5
Daniel Ablass <daniel.ablass@plan.de>
2024-07-19 16:46:51 +0200

    [TASK] Format code.

--- Classes/Utility/QueryUtility.php
+++ Classes/Utility/QueryUtility.php
@@ -20,6 +20,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
+use function is_string;
 
 /**
  * Class QueryUtility
@@ -37,25 +38,31 @@ class QueryUtility
     public static function debug(QueryInterface $query, ?string $title = null): void
     {
         $queryParser = GeneralUtility::makeInstance(Typo3DbQueryParser::class);
-        $sql = $queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL() . ';';
-        $parameters = $queryParser->convertQueryToDoctrineQueryBuilder($query)->getParameters();
+        $sql = $queryParser->convertQueryToDoctrineQueryBuilder($query)
+                ->getSQL() . ';';
+        $parameters = $queryParser->convertQueryToDoctrineQueryBuilder($query)
+            ->getParameters();
         $searchArray = array_keys($parameters);
-        array_walk($searchArray, static function (&$value) {
+        array_walk($searchArray, static function(&$value) {
             $value = ':' . $value;
         });
         $replaceArray = array_values($parameters);
-        array_walk($replaceArray, static function (&$value) {
+        array_walk($replaceArray, static function(&$value) {
             if (is_string($value)) {
                 $value = '"' . $value . '"';
             }
         });
 
         $template = GeneralUtility::makeInstance(StandaloneView::class);
-        $template->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName('EXT:psb_debug/Resources/Private/Templates/Message.html'));
+        $template->setTemplatePathAndFilename(
+            GeneralUtility::getFileAbsFileName('EXT:psb_debug/Resources/Private/Templates/Message.html')
+        );
         $template->assignMultiple([
             // Revert search order so that "value10" gets replaced before "value1"!
             'message' => str_replace(array_reverse($searchArray), array_reverse($replaceArray), $sql),
-            'title'   => $title ? : 'Query from ' . BackTraceUtility::getClass(2) . ':' . BackTraceUtility::getFunction(2),
+            'title'   => $title ?: 'Query from ' . BackTraceUtility::getClass(2) . ':' . BackTraceUtility::getFunction(
+                    2
+                ),
         ]);
 
         echo $template->render();
